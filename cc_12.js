9e09bba2c6b1d6f4cf15ec7062868dd75adf9eb8
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-03-02 17:41:11 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -5,12 +5,24 @@ const dashboardQuery = document.querySelector("#dashboard"); // Using Query Sele
 
 const revenueCard = document.createElement("div"); // Creating a new metric card in "div" called Revenue
 
-revenueCard.setAttribute("class,", "metric-card"); // Added class attribute for Revenue metric cards
+revenueCard.setAttribute("class", "metric-card"); // Added class attribute for Revenue metric cards
 revenueCard.setAttribute("id", "revenue-card"); // Added Id attribute to Revenue metric card
 
 revenueCard.innerHTML = `
 <h3>Revenue</h3>
-<p>$ 0</p>
+<p>$ 0 </p>
 `; // Populating the Revenue metric card
 
 dashboardId.appendChild(revenueCard); // appending the Revenue metric card to the dashboard container
+
+// Task 2 Updating Dashboard Metrics – Working with NodeLists and Arrays
+
+const metricCards = document.querySelectorAll(".metric-card"); // Selecting all elements within metric card class
+
+const metricCardsArray = [...metricCards]; // Converting Nodelist to an array
+
+metricCardsArray.forEach(card => {
+    card.innerText += " - Updated"; //
+    card.style.backgroundColor = `green`; // Changing background color for each metric card to green
+})
+
